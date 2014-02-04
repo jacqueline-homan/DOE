8fd97977ff0a20b2195b61fbf420f6e307679388
jacqueline-homan <jacquelinehoman7@gmail.com>
2014-02-04 00:00:46 -0500

    Added 'require Capybara' to spec_helper file

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,11 @@
 class Customer < ActiveRecord::Base
   attr_accessor :email
-  def new_user
-  	@user = User.new(name: "Joe Customer", email: "joe@example.com")
+  def new_customer
+  	@customer = Customer.new(params[:first_name, :last_name, :email])
   end
 
-  def user
-  	@user = User.find(params[:email])
+  private
+  def customer_params
+  	@customer = Customer.find(params[:email])
   end
 end
